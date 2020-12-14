97b35c578b020a0bca4b9ddaf395953b7315005c
Nthcrjk <arfilippov@bk.ru>
2020-12-14 10:25:55 +0300

    Simple MVP architecture Created

---
+++ app/src/main/java/com/example/rickandmortycharacters/presentations/presenter/MainPresenter.java
@@ -0,0 +1,14 @@
+package com.example.rickandmortycharacters.presentations.presenter;
+
+import com.example.rickandmortycharacters.presentations.view.MainView;
+
+import moxy.InjectViewState;
+import moxy.MvpPresenter;
+
+@InjectViewState
+public class MainPresenter extends MvpPresenter<MainView> {
+
+    public MainPresenter(){
+
+    }
+}
---
+++ app/src/main/java/com/example/rickandmortycharacters/ui/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rickandmortycharacters.ui.activity;
+
+import android.os.Bundle;
+
+import com.example.rickandmortycharacters.R;
+import com.example.rickandmortycharacters.presentations.presenter.MainPresenter;
+import com.example.rickandmortycharacters.presentations.view.MainView;
+
+import moxy.MvpAppCompatActivity;
+import moxy.presenter.InjectPresenter;
+
+public class MainActivity extends MvpAppCompatActivity implements MainView {
+
+    @InjectPresenter
+    MainPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
